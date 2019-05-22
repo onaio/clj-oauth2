7d6a101bbe2417a59384f348d26b5f92a92c4094
John Mwashuma <jmwashuma@live.com>
2019-05-22 16:45:38 +0300

    Fix return url

--- src/clj_oauth2/client.clj
+++ src/clj_oauth2/client.clj
@@ -15,13 +15,13 @@
            redirect-uri
            scope
            response-type]}
-   & {:keys [state redirect-client-id]}]
+   & {:keys [state return_url]}]
   (let [uri (uri/uri->map (uri/make authorization-uri) true)
         query (assoc (:query uri)
                 :client_id client-id
                 :redirect_uri (cond-> redirect-uri
-                                (-> redirect-client-id str/blank? not)
-                                (str "?redirect-client-id=" redirect-client-id))
+                                (-> return_url str/blank? not)
+                                (str "?return_url=" return_url))
                 :response_type (or response-type "code"))
         query (if state (assoc query :state state) query)
         query (if scope
